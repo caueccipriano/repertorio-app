7f01e8c2a90325b7f19f1896ff9c1469507ae0fb
caueccipriano <caueccipriano@gmail.com>
2026-09-17 22:59:11 -0300

    feat: connect browser speech synthesis

---
+++ lib/core/audio/study_speech_web.dart
@@ -0,0 +1,18 @@
+import 'dart:js_interop';
+
+@JS('repertorioSpeech.supported')
+external JSBoolean _supported();
+
+@JS('repertorioSpeech.speak')
+external JSPromise<JSString> _speak(JSString text);
+
+@JS('repertorioSpeech.stop')
+external void _stop();
+
+bool get studySpeechSupported => _supported().toDart;
+
+Future<void> speakStudyText(String text) async {
+  await _speak(text.toJS).toDart;
+}
+
+void stopStudySpeech() => _stop();
